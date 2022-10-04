dee177c8e98b160806acd3e59206454b169d84a6
Julian Ewers <ewerspej@tcd.ie>
2022-10-04 16:22:24 +0200

    started implementation of ProgressBar and downgraded SkiaSharp to a compatible version

--- src/epj.ProgressBar.Maui/ProgressBar.cs
+++ src/epj.ProgressBar.Maui/ProgressBar.cs
@@ -1,8 +1,56 @@
-﻿using SkiaSharp.Views.Maui.Controls;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
 
 namespace epj.ProgressBar.Maui;
 
 public class ProgressBar : SKCanvasView
 {
+    private SKCanvas _canvas;
+    private SKRect _drawRect;
+    private SKImageInfo _info;
 
+    public Color BaseColor
+    {
+        get => (Color)GetValue(BaseColorProperty);
+        set => SetValue(BaseColorProperty, value);
+    }
+
+    public static readonly BindableProperty BaseColorProperty = BindableProperty.Create(nameof(BaseColor), typeof(Color), typeof(ProgressBar), Colors.LightGray, propertyChanged: OnBindablePropertyChanged);
+
+    private static void OnBindablePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        ((ProgressBar)bindable).InvalidateSurface();
+    }
+
+    public ProgressBar()
+    {
+        IgnorePixelScaling = false;
+    }
+
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+    {
+        base.OnPaintSurface(e);
+
+        _canvas = e.Surface.Canvas;
+        _canvas.Clear();
+
+        _info = e.Info;
+
+        _drawRect = new SKRect(0, 0, _info.Width, _info.Height);
+
+        DrawBase();
+    }
+
+    private void DrawBase()
+    {
+        using var basePath = new SKPath();
+        basePath.AddRect(_drawRect);
+        _canvas.DrawPath(basePath, new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = BaseColor.ToSKColor(),
+            IsAntialias = true
+        });
+    }
 }
